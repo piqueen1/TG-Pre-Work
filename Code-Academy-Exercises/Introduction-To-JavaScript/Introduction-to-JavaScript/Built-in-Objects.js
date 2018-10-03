6dd4047ba9f133c8008be5da55eee82e7e28a4ac
Finn Thye <piqueen@gmail.com>
2018-10-03 08:19:24 -0600

    JS intro Built-in Objects

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Built-in-Objects.js
@@ -0,0 +1,6 @@
+//Built-in Objects
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
+
+//if you wanted to perform more complex mathematical operations than arithmetic, JavaScript has the built-in Math object.
