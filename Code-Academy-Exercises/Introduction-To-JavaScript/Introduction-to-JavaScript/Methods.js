7ab312b9b8e2ba47f9820111429b3f5ed7e76174
Finn Thye <piqueen@gmail.com>
2018-10-03 08:15:12 -0600

    JS intro Methods

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Methods.js
@@ -0,0 +1,7 @@
+//6. Built-in Methods
+// Log Codecademy in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+// Use a string method to log the following statment without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+//We call, or use, these methods by appending an instance with a period (the dot operator), the name of the method, and opening and closing parentheses: ie. 'example string'.methodName().
